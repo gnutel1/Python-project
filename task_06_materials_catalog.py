5e789e349c7591aa6faab6f1346f15cf50dfe188
gnutel1 <149432295+gnutel1@users.noreply.github.com>
2026-04-23 21:11:21 +0300

    Update task_06_materials_catalog.py

--- task_06_materials_catalog.py
+++ task_06_materials_catalog.py
@@ -1,9 +1,13 @@
+#вводим список материалов
 mat = ['titan', 'shlakobeton', 'glina', 'nenutonovskaya zhidkost', 'cement']
+#выводим первый последний и среднии материалы из списка
 print(mat[:1])
 a = len(mat) - 1
 print(mat[a:])
 print(mat[1:a])
+#добавляем новые материалы и удаляем старый
 mat.append("gudron")
 mat.append('zhizha')
 mat.pop(1)
+#выводим конечный результат
 print(mat, len(mat))
