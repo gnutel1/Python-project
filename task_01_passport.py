386be3edf48fb84aaf0c9a626bf67175ea162889
gnutel1 <149432295+gnutel1@users.noreply.github.com>
2026-04-23 20:53:27 +0300

    Create task_01_passport.py

---
+++ task_01_passport.py
@@ -0,0 +1,20 @@
+student_name = 'Лукин Артем Алексеевич'
+group_number = '3150801/10101'
+project_name = 'ТРК Титан Арена'
+floors = 5
+height = 15.5
+is_residential = False
+construction_year = 2001
+print('=== Паспорт строительного объекта ===')
+print('Составитель: ', student_name)
+print('Группа: ', group_number)
+print('')
+print('Объект: ', project_name)
+print('Этажность: ', floors, ' этажей')
+print('Высота: ', height, ' м')
+if is_residential == True:
+    print('Тип: Жилой')
+else:
+    print('Тип: Нежилой')
+print('Год постройки: ', construction_year)
+
