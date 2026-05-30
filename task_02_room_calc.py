3ccfcdeed6d84330ed5b97eb835c6eeacca178ee
gnutel1 <149432295+gnutel1@users.noreply.github.com>
2026-05-30 10:47:11 +0300

    Update task_02_room_calc.py

--- task_02_room_calc.py
+++ task_02_room_calc.py
@@ -1,14 +1,20 @@
-#вводим переменные
-lenght = 3.5
-width = 2.5
-height = 3.3423231
-#выполняем расчет
-pl_pola = lenght*width
-pl_sten1 = height*lenght
-pl_sten2 = height*width
-obsch_pl_sten = (pl_sten1 + pl_sten2) * 2
-obem = lenght*width*height
-st_pok_m = 125
-itog_stoim = st_pok_m*obsch_pl_sten
-#округляем и выводим итог. значение
-print(round(itog_stoim, 2))
+Задача 2: Параметры помещения
+Размеры помещения (в метрах)
+length = 3.5    # длина 
+width = 2.5     # ширина
+height = 3.3423231  # высота
+Вычисление площадей и объёма 
+pl_pola = length * width                       
+pl_sten1 = height * length                      
+pl_sten2 = height * width                       
+obsch_pl_sten = (pl_sten1 + pl_sten2) * 2       
+obem = length * width * height                 
+Стоимость покраски 
+st_pok_m = 125                                 
+itog_stoim = st_pok_m * obsch_pl_sten           
+Вывод результатов 
+print('=== Параметры помещения ===')
+print(f'Площадь пола:   {round(pl_pola, 2)} м²')
+print(f'Площадь стен:   {round(obsch_pl_sten, 2)} м²')
+print(f'Объём:          {round(obem, 2)} м³')
+print(f'Стоимость покраски стен: {round(itog_stoim, 2)} руб.')
